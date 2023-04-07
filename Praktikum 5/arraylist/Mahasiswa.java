8acf5a9bf91db687bed1497e9f5a9d955168ceda
wildankhafichudin <115627076+wildankhafichudin@users.noreply.github.com>
2023-04-07 14:44:06 +0700

    Add files via upload

---
+++ Praktikum 5/arraylist/Mahasiswa.java
@@ -0,0 +1,35 @@
+package arraylist;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+class Mahasiswa {
+    private String nama;
+    private String nim;
+    private ArrayList<Double> nilai;
+
+    public Mahasiswa(String nama, String nim, ArrayList<Double> nilai) {
+        this.nama = nama;
+        this.nim = nim;
+        this.nilai = nilai;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public ArrayList<Double> getNilai() {
+        return nilai;
+    }
+
+    public double hitungIPK() {
+        double total = 0;
+        for (double n : nilai) {
+            total += n;
+        }
+        return total / nilai.size();
+    }
+}
